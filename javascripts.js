09e9b4d34d0c3e85388e9ba5cc74e0454d64fb4e
Chandan mishra <68228840+chandanmishrak@users.noreply.github.com>
2022-09-06 05:42:41 +0545

    Add files via upload

---
+++ javascripts.js
@@ -0,0 +1,31 @@
+//how to work console 
+console.log("hello world from javaScript course")
+let a = 14 //a contains 14
+console.log(a)
+
+a= "chandan"
+console.log(a)
+
+// var, let, const---------- (lesson-1)
+
+// var A = 10; //var is not recomended plz use let or const
+// let b = "Chandan";
+// var c = null
+// var d = undefined
+
+let A = 10;
+let b = "Chandan";
+const writer = "chandan";
+// const program; //it will throw me an error because not declaration const
+// let writer = "chandan"; // throws an error because constant cannot be change
+// let b = 11; // b has already been declared 
+b = 11; // you can change let in same container
+let c = null
+let d = undefined
+{
+    let b = 'what'
+    console.log(b)
+}
+console.log(b)
+
+//primitives objects ------------ (lesson-2)
